adc511a99967d92a7234b80b48fbd1f3a500ba4b
CrioUser <crio-user@criodo.com>
2023-02-24 17:17:30 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -4,24 +4,44 @@ async function init() {
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
 
+
   //Updates the DOM with the cities
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
+    
   });
 }
-
 //Implementation of fetch call
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    let response= await fetch("http://13.232.57.61:8082/cities");
+    let user= await response.json();
+    return user;
+  } catch(error){
+    return null;
+  }
+ 
+ 
 }
-
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
-
+  let createCity=document.createElement("div")
+  createCity.className= "col-12 col-sm-6 col-lg-3 mb-3";
+  createCity.innerHTML=`
+  <a href="pages/adventures/?city=${id}" , id="${id}">
+    <div class="tile">
+      <div class="tile-text text-center text-white">
+        <h4>${city}</h4>
+        <p>${description}</p>
+      </div>
+      <img class="image-responsive" src="${image}">
+    </div>
+  </a>
+ `;
+document.getElementById("data").appendChild(createCity)
 }
-
 export { init, fetchCities, addCityToDOM };
